2a5375392eb0dbc34bca566ab073af2150decfda
HenriqRocha <hrocha2703@gmail.com>
2025-12-16 01:18:11 -0300

    correção testes 7

--- src/test/java/com/example/echo/EchoApplicationTests.java
+++ src/test/java/com/example/echo/EchoApplicationTests.java
@@ -1,33 +1,28 @@
 package com.example.echo;
 
+import com.example.echo.service.EmailService;
+import com.example.echo.service.externo.ExternoClient;
+import com.example.echo.service.externo.EquipamentoClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
-@SpringBootTest(properties = {
-
-		"api.externo.url=http://url-teste-mock.com",
-
-
-		"spring.mail.host=localhost",
-		"spring.mail.port=1025",
-		"spring.mail.username=teste",
-		"spring.mail.password=teste",
-		"spring.mail.properties.mail.smtp.auth=false",
-		"spring.mail.properties.mail.smtp.starttls.enable=false",
-
-		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
-		"spring.datasource.driverClassName=org.h2.Driver",
-		"spring.datasource.username=sa",
-		"spring.datasource.password=",
-		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
-})
+@SpringBootTest
 @AutoConfigureTestDatabase
 class EchoApplicationTests {
+
+	@MockBean
+	private ExternoClient externoClient;
+
+	@MockBean
+	private EquipamentoClient equipamentoClient;
+
+	@MockBean
+	private EmailService emailService;
 
 	@Test
 	void contextLoads() {
 
 	}
-
 }
