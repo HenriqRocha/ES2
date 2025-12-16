09e3e2d811a6e7fbe216b109d876c0e11b68c268
HenriqRocha <hrocha2703@gmail.com>
2025-12-16 01:04:55 -0300

    correção testes 5

--- src/test/java/com/example/echo/EchoApplicationTests.java
+++ src/test/java/com/example/echo/EchoApplicationTests.java
@@ -2,14 +2,15 @@ package com.example.echo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @SpringBootTest(properties = "api.externo.url=http://url-teste-mock.com")
+@AutoConfigureTestDatabase
 class EchoApplicationTests {
 
 	@Test
 	void contextLoads() {
-
+		// Agora ele usa H2 em memória e tem a URL externa definida
 	}
 
 }
