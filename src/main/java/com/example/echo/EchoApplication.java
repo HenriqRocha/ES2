01f5fbead5deed06a2f4a24629a92f262be71059
HenriqRocha <hrocha2703@gmail.com>
2025-12-16 00:25:31 -0300

    correções testes 2

--- src/main/java/com/example/echo/EchoApplication.java
+++ src/main/java/com/example/echo/EchoApplication.java
@@ -2,12 +2,19 @@ package com.example.echo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EchoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EchoApplication.class, args); 
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
