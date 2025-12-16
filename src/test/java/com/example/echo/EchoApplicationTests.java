50742d48caaee45aa61b2c1fc17863bdc7f05643
HenriqRocha <hrocha2703@gmail.com>
2025-12-16 00:54:59 -0300

    correção testes 4

--- src/test/java/com/example/echo/EchoApplicationTests.java
+++ src/test/java/com/example/echo/EchoApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.echo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(properties = "api.externo.url=http://url-teste-mock.com")
 class EchoApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
-	
+
 }
